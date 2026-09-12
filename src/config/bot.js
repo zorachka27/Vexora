9b2fa7d934ce1919d1df92d298d86de42c9e6ef3
zorachka27 <zorachka27@gmail.com>
2026-09-12 15:00:18 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,7 +93,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#000000",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
